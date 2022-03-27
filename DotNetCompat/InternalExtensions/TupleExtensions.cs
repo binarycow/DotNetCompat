e5afd6f0b4595f9dd8da512c4ac21e1b35a53048
Mike Christiansen <michael.a.christiansen10@gmail.com>
2022-03-27 09:21:57 -0400

    Initial commit

---
+++ DotNetCompat/InternalExtensions/TupleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DotNetCompat.InternalExtensions;
+
+internal static class TupleExtensions
+{
+    public static bool Try<T>(this (bool Success, T Result) tuple, out T result)
+    {
+        result = tuple.Result;
+        return tuple.Success;
+    }
+}
---
+++ DotNetCompat/NetStandard21/Stack.cs
@@ -0,0 +1,18 @@
+﻿#if !NETSTANDARD2_1_OR_GREATER
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using DotNetCompat.InternalExtensions;
+
+namespace DotNetCompat.NetStandard21;
+
+public static class Stack
+{
+    public static bool TryPop<T>(this Stack<T> stack, [MaybeNullWhen(false)] out T item) 
+        => (stack.Count is 0
+            ? (false, default)
+            : (true, stack.Pop())
+        ).Try(out item);
+}
+
+#endif
